a7e19ffe7d4270bc7f283fd0793eaf4d7b33d1ad
Rihan Farih Bunyamin <Rihan Farih Bunyamin@DESKTOP-9Q8PJIJ>
2020-12-02 22:35:21 +0700

    project2

---
+++ rihanfarih/project2(chapter12.5)/src/id/ac/its/rihan165/LabelTest.java
@@ -0,0 +1,12 @@
+package id.ac.its.rihan165;
+
+import javax.swing.JFrame;
+
+public class LabelTest {
+	public static void main(String[] args) {
+		LabelFrame labelFrame = new LabelFrame();
+		labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		labelFrame.setSize(320, 250);
+		labelFrame.setVisible(true);
+	}
+}
